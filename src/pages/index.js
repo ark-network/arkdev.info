f42b589bf3ed3493d27e894e058992a9ccb5f8c2
tiero <3596602+tiero@users.noreply.github.com>
2024-07-23 16:45:59 +0200

    new landing page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,70 @@
+// index.js
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import styles from './index.module.css';
-import ArkLogo from '@site/static/img/ark-logo.png'; 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
 
-const Feature = ({ title, description, buttonTitle, buttonLink }) => (
-  <div className={clsx('col col--4', styles.feature)}>
-    <h2>{title}</h2>
-    <p>{description}</p>
-    <Link className="button button--primary" to={buttonLink}>
-      {buttonTitle || 'Learn more'}
+const FeatureItem = ({icon, title, description, buttonText, buttonLink}) => (
+  <div className={styles.featureItem}>
+    <img src={icon} alt={title} className={styles.featureIcon} />
+    <h3 className={styles.featureTitle}>{title}</h3>
+    <p className={styles.featureDescription}>{description}</p>
+    <Link to={buttonLink} className={styles.featureButton}>
+      {buttonText}
     </Link>
   </div>
 );
 
+const Separator = () => <div className={styles.separator}></div>;
 
-const HomePage = () => {
-  const { siteConfig } = useDocusaurusContext();
 
+function HomepageHeader() {
   return (
-    <Layout>
-      <main>
-        <div className={styles.hero}>
-          <div className="container">
-            <img src={ArkLogo} alt="Ark Logo" className={styles.hero__logo} />
-            <h1 className="hero__title">Ark Developer Hub</h1>
-            <p className="hero__subtitle">cheap, fast and confidential bitcoin transactions</p>
-          </div>
-        </div>
-        <div className={styles.features}>
-          <div className="container">
-            <div className="row">
-              <Feature
-                title="Learn Ark"
-                buttonLink={'/docs/learn/intro'}
-                description="Dive into the world of Ark with comprehensive learning resources tailored for developers."
-              />
-              <Feature
-                title="Start using Ark"
-                buttonTitle={'Get Started'}
-                buttonLink={'/docs/user/intro'}
-                description="Use ARK for easy, cheap and fast bitcoin transactions"
-              />
-              <Feature
-                title="Provide Liquidity to an Ark"
-                buttonTitle={'Run an ASP'}
-                buttonLink={'/docs/provider/intro'}
-                description="Join the network of service providers to facilitate seamless bitcoin transactions."
-              />
-            </div>
-          </div>
-        </div>
+    <header className={styles.heroBanner}>
+      <div className={styles.heroContent}>
+        <img src="/img/ark-cubes.png" alt="Ark Cubes" className={styles.arkCubes} />
+        <h1 className={styles.heroTitle}>Build on Ark</h1>
+        <p className={styles.heroSubtitle}>Cheap, fast and confidential Bitcoin transactions</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Welcome to ${siteConfig.title}`}
+      description="Build on Ark - Cheap, fast and confidential Bitcoin transactions">
+      <HomepageHeader />
+      <main className={styles.main}>
+        <section className={styles.features}>
+          <FeatureItem
+            icon="/img/learn-ark.png"
+            title="Learn Ark"
+            description="Dive into the world of Ark with comprehensive learning resources tailored for developers."
+            buttonText="Learn more"
+            buttonLink="/docs/learn/intro"
+          />
+          <Separator />
+          <FeatureItem
+            icon="/img/use-ark.png"
+            title="Start Using Ark"
+            description="Use ARK for easy, cheap and fast bitcoin transactions."
+            buttonText="Build now"
+            buttonLink="/docs/user/intro"
+          />
+          <Separator />
+          <FeatureItem
+            icon="/img/provide-liquidity.png"
+            title="Provide Liquidity to Ark"
+            description="Join the network of service providers to facilitate seamless bitcoin transactions."
+            buttonText="Learn more"
+            buttonLink="/docs/provider/intro"
+          />
+        </section>
       </main>
     </Layout>
   );
-};
-
-export default HomePage;
+}
